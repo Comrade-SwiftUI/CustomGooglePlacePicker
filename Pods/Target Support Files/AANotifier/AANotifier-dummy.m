753544bcfecefa375e7ce2c2508a995bf2dcb933
Bhavesh Chavda <chavdabhavesh99@gmail.com>
2020-01-23 10:35:36 +0530

    removed podsfiles

--- Pods/Target Support Files/AANotifier/AANotifier-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AANotifier : NSObject
-@end
-@implementation PodsDummy_AANotifier
-@end
--- Pods/Target Support Files/AAViewAnimator/AAViewAnimator-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AAViewAnimator : NSObject
-@end
-@implementation PodsDummy_AAViewAnimator
-@end
